cc5d0895f5797de3d3441696e2692a6755ce60fc
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-16 15:35:49 -0400

    Done

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -9,8 +9,13 @@ class OwnersController < ApplicationController
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners/:id' do 
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
@@ -23,7 +28,12 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  post '/owners/:id' do 
-   
+  post '/owners' do 
+   @owner = Owner.create(params[:owner])
+   if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
+
 end
--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -98,7 +98,7 @@ describe "Pets Controller" do
 
     it "edit's the pet's owner with a new owner" do
       visit "/pets/#{@pet.id}/edit"
-      fill_in "owner_name", :with => "Samantha"
+      fill_in "owner[name]", :with => "Samantha"
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Samantha")
     end
